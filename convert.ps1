17ffc69bd1c7198aac78e9657be77dba3e56fa24
bouni <bouni@owee.de>
2014-04-17 14:38:27 +0200

    added png's of all svg's and a powershell script to convert all svg's to png's

---
+++ convert.ps1
@@ -0,0 +1,9 @@
+﻿# convert all svg's to png's
+# make sure that inkscape is in path variable
+
+$svgs = Get-ChildItem "." -Filter *.svg
+
+foreach($svg in $svgs) {
+    $basename = $svg.basename
+    inkscape -z -e "${basename}.png" -w 3072 $svg
+}
